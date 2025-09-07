e66fa1bfa51e83858f4d1d6e0b9d5e8e7dede1e5
Henrique Scander Coelho <118944354+Mr-SweetRice@users.noreply.github.com>
2025-09-07 13:36:16 -0300

    resolv

--- S2_bot_copy/SHControl.h
+++ S2_bot_copy/SHControl.h
@@ -10,17 +10,30 @@ float integral=0;
 float derivative=0;
 uint16_t pos =0;
 
-int control(float linePosition, uint16_t target,float p, float i,float d){
+int control(int16_t linePosition, uint16_t target,float p, float i,float d,bool* curva){
     unsigned long now = millis();
     error =target-linePosition; 
     unsigned long dt = (now - last_time);
     integral = integral+ error*dt; 
     derivative = (abs(error)-abs(last_error));
+    if( labs(derivative)>ERRO_CURV){*curva=1;}else{*curva=0;}
     integral = constrain(integral,-20000000, 20000000);
     int u =round((p*error*dt + i*integral + d*derivative));
     last_time = now;
     last_error = error;
-  
+    // Serial.print(p);
+    // Serial.print(" | ");
+    // Serial.print(linePosition);
+    // Serial.print(" | ");
+    // Serial.print(dt);
+    // Serial.print(" | ");
+    // Serial.println(error);
+    // Serial.print(" | ");
+    // Serial.print((p*error*dt + i*integral + d*derivative));
+    // Serial.print(" | ");
+    // Serial.println(u);
+    
+    
     return u;
 }
 
